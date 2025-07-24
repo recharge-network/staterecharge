7fdc9ad4420ab97d1e2993be91ec3180d781c370
recharge-network <myrechargenetwork@gmail.com>
2025-07-24 04:16:19 -0400

    Update and rename api/login.php to api/api/login.php

--- api/api/login.php
+++ api/api/login.php
@@ -1,19 +1,21 @@
 <?php
 header('Content-Type: application/json');
 
-// Dummy login data (future me DB ya API se check hoga)
+// Static user list for testing
 $valid_users = [
     "admin" => "1234",
     "demo" => "demo123"
 ];
 
+// Read POST values
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 
+// Check credentials
 if (isset($valid_users[$username]) && $valid_users[$username] === $password) {
     echo json_encode([
         "success" => true,
-        "message" => "Login successful!"
+        "message" => "Login successful"
     ]);
 } else {
     echo json_encode([
